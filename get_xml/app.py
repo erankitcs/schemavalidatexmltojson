1df1049e3fd20d8f05f8a2d871be17c7b8419bb4
er.ankit.cs <er.ankit.cs@gmail.com>
2021-10-11 02:56:47 +1100

    xml payload api.

---
+++ get_xml/app.py
@@ -0,0 +1,81 @@
+import json
+import sys
+import uuid
+import os
+import boto3
+from datetime import datetime
+
+PAYLOAD_BUCKET = os.getenv('PAYLOAD_BUCKET')
+DYNAMODB_TABLE = os.getenv('DYNAMODB_TABLE')
+EVENTBRIDGE = os.getenv('EVENTBRIDGE')
+REGION=os.getenv('REGION')
+
+s3 = boto3.resource('s3', region_name=REGION)
+events_client = boto3.client('events')
+
+def upload_payload(payload,objkey):
+    """
+    This function will uplaod payload to S3 bucket.
+    
+    """
+    bucket = s3.Bucket(PAYLOAD_BUCKET)
+    response = bucket.put_object(payload)
+    return True
+
+def lambda_handler(event, context):
+    """Lambda function to check xml payload size and route request to EventBridge.
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+    print(event)
+
+    body     = event["body"]
+    client   = event["body"]["metadata"]["client"]
+    payload  = event["body"]["data"]
+    uniqueid = str(uuid.uuid4())
+    reference_id = uniqueid+"-"+client
+    print("checking payload size coming from the request.")
+    payload_size = sys.getsizeof(payload)/1024
+    if payload_size > 250 :
+        print("Payload is too large for Eventbridge. Payload size-  %s", payload_size)
+        upload_payload(payload,reference_id)
+        body["data"] = ""
+    else:
+        print("Good to go for eventbridge directly. Payload size-  %s", payload_size)
+    
+    print("Calling Event Bridge with request payload.")
+    Entries=[
+        {
+            'Time': datetime.now,
+            'Source': 'get_xml_lambda',
+            'Resources': [
+                'string',
+            ],
+            'DetailType': 'Message',
+            'Detail': json.dumps(
+                body
+            ),
+            'EventBusName': EVENTBRIDGE,
+            'TraceHeader': reference_id
+        },
+    ]
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "XML is submitted for schema validation and further json conversion.",
+            "reference_id": reference_id
+        }),
+    }
